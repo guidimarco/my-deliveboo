c17f6ce399d82d7e8ffddb6dcee76746189cfec7
Marco Guidi <mark.88g@gmail.com>
2021-03-07 15:51:37 +0100

    stilata pagina login

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield('page-title', 'DeliveBoo')</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- FontAwesome CDN -->
+        <script src="https://kit.fontawesome.com/43febffcb7.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <!-- header (logo && nav bar) -->
+        @include('partials.guest.header')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+    </body>
+
+</html>
